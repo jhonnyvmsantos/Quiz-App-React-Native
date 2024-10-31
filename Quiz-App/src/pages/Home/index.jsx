f9b0fb57fc367a837fc72a8f3cb48b54d570f5d6
DioneKx <mjhonnyvieira@gmail.com>
2024-10-31 15:42:58 -0300

    Jhonny: Github

--- Quiz-App/src/pages/Home/index.jsx
+++ Quiz-App/src/pages/Home/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { styles } from './style';
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export function HomePage() {
 
   const navigation = useNavigation();
+
+  const gitTransfer = async () => {
+    const url = "https://github.com/jhonnyvmsantos"
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      console.warn(`Don't know how to open this URL: ${url}`);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -14,13 +25,13 @@ export function HomePage() {
       </View>
       <View style={styles.content}>
         <TouchableOpacity style={styles.button} onPress={() => {
-          navigation.navigate("Quiz", {redirector: "Home", limited: 0})
+          navigation.navigate("Quiz", { redirector: "Home", limited: 0 })
         }}>
           <Text style={styles.text}>ALL-ITEMS</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.button, {backgroundColor: "black"}]}>
-          <Text style={[styles.text, {color: "white"}]}>GITHUB</Text>
+        <TouchableOpacity onPress={gitTransfer} style={[styles.button, { backgroundColor: "black" }]}>
+          <Text style={[styles.text, { color: "white" }]}>GITHUB</Text>
         </TouchableOpacity>
       </View>
     </View>
